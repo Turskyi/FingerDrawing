df4215ab565f2deb9d47e1162a27013db1bb8f70
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-06-29 17:43:10 +0300

    Initial commit

---
+++ app/src/main/java/ua/turskyi/fingerdrawing/CanvasActivity14.kt
@@ -0,0 +1,53 @@
+package ua.turskyi.fingerdrawing
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.*
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+class CanvasActivity14 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    class SignatureView(context: Context, attributeSet: AttributeSet?) : View(context, attributeSet) {
+        private val paint: Paint = Paint()
+        private val path: Path = Path()
+
+        init {
+            paint.color = ContextCompat.getColor(
+                    context,
+                    R.color.colorPrimary
+            )
+            paint.style = Paint.Style.STROKE
+            paint.strokeJoin = Paint.Join.ROUND
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.strokeWidth = 10F
+        }
+
+        override fun onDraw(canvas: Canvas) {
+            super.onDraw(canvas)
+            canvas.drawPath(path, paint)
+        }
+
+        @SuppressLint("ClickableViewAccessibility")
+        override fun onTouchEvent(event: MotionEvent): Boolean {
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> path.moveTo(event.x, event.y)
+                MotionEvent.ACTION_MOVE -> {
+                    path.lineTo(event.x, event.y)
+                    invalidate()
+                }
+                MotionEvent.ACTION_UP -> {
+                }
+            }
+            return true
+        }
+    }
+}
